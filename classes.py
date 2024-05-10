a26aaafc697c14184f91e602fbacdf165d9ab7db
Andre <andrefd1@al.insper.edu.br>
2024-05-10 18:56:22 -0300

    ClasseCarro

--- classes.py
+++ classes.py
@@ -5,4 +5,12 @@ from config import *
 from imagens import *
 
 
+#Classe do carro:
+class Carro(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.transform.rotate(pygame.transform.scale(car_image, (150, 150)), 270)#Rotacionando para virar o carro na direcao certa
+        self.rect = self.image.get_rect(center=(WIDTH // 2, HEIGHT - 100))#Definindo o retângulo que esta port volta da imagem.
+        self.speed = 5 #Velocidade do carro
+
 
