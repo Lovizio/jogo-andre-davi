17f80f12be66026bf20f71263967fe3d979fe87c
Lovizio <davifl@al.insper.edu.br>
2024-05-13 00:58:21 -0300

    ajustando as classes

--- classes.py
+++ classes.py
@@ -14,3 +14,31 @@ class Carro(pygame.sprite.Sprite):
         self.speed = 5 #Velocidade do carro
 
 
+def update(self, keys):
+        # Ver se a tecla da seta para esquerda foi apertada
+        if keys[pygame.K_LEFT]:
+        # Ver o limite
+            if self.rect.left > 0:
+            # Mover para a esquerda
+                self.rect.x -= self.speed
+
+    # Verifica se a tecla da seta para direita foi apertada
+        if keys[pygame.K_RIGHT]:
+        # Ver o limite
+            if self.rect.right < WIDTH:
+            # Move para a direita
+                self.rect.x += self.speed
+
+    # Ver se a tecla da seta para cima foi apertada
+        if keys[pygame.K_UP]:
+        # Ver o limite
+            if self.rect.top > 0:
+            # Move para cima
+                self.rect.y -= self.speed
+
+    # Ver se a tecla da seta para baixo foi apertada
+        if keys[pygame.K_DOWN]:
+        # Ver o limite
+            if self.rect.bottom < HEIGHT:
+            # Move para baixo
+                self.rect.y += self.speed
