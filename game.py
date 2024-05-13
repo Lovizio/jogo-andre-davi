d3ef92b5469fadee9408a51558d896f8f7e92a5b
Andre <andrefd1@al.insper.edu.br>
2024-05-13 16:15:57 -0300

    FuncaoBarraVida

--- game.py
+++ game.py
@@ -3,3 +3,7 @@ import pygame
 from config import *
 from classes import Carro, Obstaculo
 from imagens import background_image
+
+def barra_vida(current_health):
+    pygame.draw.rect(window, GREEN, (10, 10, 200 * (current_health / 5), 20))
+
